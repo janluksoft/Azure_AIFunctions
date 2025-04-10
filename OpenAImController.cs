1701e65298581981ea4ef1691ef26f5d063543d2
janluksoft <janluksoft@interia.pl>
2025-04-10 11:47:19 +0200

    Project files

---
+++ OpenAImController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OpenAI.Chat;
+using pInvestors.Data;
+using pInvestors.Models;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace pInvestors.Controllers;
+
+public class OpenAImController : Controller
+{
+    private readonly CAzureOpenAIUtils _openAIUtils;
+    private readonly pInvestorsContext _context;
+    private readonly MyLog3 _myLog;
+
+    public OpenAImController(MyLog3 myLog)
+    {
+        _openAIUtils = new CAzureOpenAIUtils();
+        _myLog = myLog;
+
+        if (_myLog.status < 2)
+        {
+            RedirectToAction("LogFailed", "OpenAI");
+            return;
+        }
+        _context = new pInvestorsContext(_myLog.connectionString);
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        if (_myLog.status < 2)
+            return View("LogFailed");
+        return View("Index");
+    }
+
+
+    /// <summary>
+    /// The function returns the answer from AI to the given userQuestion. It triggers 
+    /// different types of actions depending on askType. 
+    /// You need to use an Azure account and a configured OpenAI service.
+    /// </summary>
+    /// <param name="userQuestion"></param>
+    /// <param name="askType"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public async Task<IActionResult> AskQuestionAjax(string userQuestion, int askType)
+    {
+        string systemText = "You are a helpful assistant.";
+        var messList = new List<ChatMessage>();
+        ChatCompletionOptions options;
+
+        // -----(2) Ask about Function Calling -----------
+        if (askType == 2)
+        {
+            options = new ChatCompletionOptions
+            {
+                Temperature = 0.0f,
+                MaxOutputTokenCount = 400
+            };
+
+            string contextText = _context._sqlCreateShareholdersTable;
+
+            if (!string.IsNullOrEmpty(contextText))
+                systemText =
+                    $" You are an assistant translating user questions into safe SQL SELECT queries" +
+                    $" When answering the user, rely on this information.";
+        }
+        // -----Other case-----------
+        else
+        {
+            options = new ChatCompletionOptions { MaxOutputTokenCount = 800 };
+        }
+
+        // ✏️ Prepare messages
+        messList.Add(new SystemChatMessage(systemText));
+        messList.Add(new UserChatMessage(userQuestion));
+
+        // Send to Azure OpenAI
+        string answer = await _openAIUtils.AskHistoryQuestion(messList, options);
+
+        // Return result
+        return Json(new { aiAnswer = answer });
+    }
+
+
+    /// <summary>
+    /// The function constructs the SQL query based on the query proposal, 
+    /// sends it to the database and returns the list from the database in JSON form.
+    /// </summary>
+    /// <param name="sqlProposal"></param>
+    /// <returns>List JSON</returns>
+    [HttpGet]
+    public async Task<IActionResult> GetShareholdersBySql([FromQuery] string sqlProposal)
+    {
+        // creates a SQL query based on the proposal
+        // The query proposal cannot be executed for security reasons. Therefore,
+        // the GetSQLCreateFromProposal function analyzes the query, extracts parameters from it
+        // and creates a secure SQL query based on them.
+        string sqlQuery = _openAIUtils.GetSQLCreateFromProposal(sqlProposal);
+
+        try
+            {
+                var list = await _context.Shareholders
+                .FromSqlRaw(sqlQuery)
+                .ToListAsync();
+                //return Json(list);
+            return Json(new { success = true, data = list });
+        }
+        catch (Exception e)
+        {
+            return Json(new { success = false, error = e.Message });
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+# Technique OpenAI - Function Calling.	
+
+
+## Overview: Function Calling
+
+The OpenAI service enables interactions between the user and AI. There are several forms of communication, a rather interesting solution is Function Calling.
+
+Function Calling (Tool Use) AI can "call" a backend function when needed. Instead of just answering with text, AI can trigger code — like getting database details, weather, etc.	Azure OpenAI already supports this! You just define a list of functions and their descriptions in the prompt.
+
+🔹 Advantages:
+ - You keep conversations natural.
+ - Users don’t need to know commands.
+ - AI helps decide when to call backend functions.
+
+## Techniques used in the program
+
+A program based on .NET 9, MVC and Entity Framework. It uses a dynamic, nice-looking DataTable and uses my own sliding windows serving as messages. One of such windows is my stand-alone GPT chat.
+
+## Program functionality
+
+The original program is de facto a <b>chat GPT written by me in a web browser</b>. 
+
+<img src="media/OpenAI_my_chat_GPT_07.png"/>
+
+In principle and function, it is very similar to the original GPT chat. However, it has the RAG (Retrieval-Augmented Generation) and Function Calling functionality.
+
+They allow not only for "text conversation" but also for operating on the database and controlling its display by asking common questions by the user. Then the user receives a powerful tool and without even knowing any SQL queries, he can obtain the same with the appropriate questions.
+
+<img src="media/App_Shareholders_Update.png"/>
+
+## Technical details
+
+The heart of the program is the OpenAImController controller, which accepts a query from the client, processes it and returns the answer. The main function AskQuestionAjax returns the answer from AI to the user's question. Triggers different types of actions depending on the askType. You must use an Azure account and a configured OpenAI service. AI returns a SQL query proposal formatted by itself. After the user agrees, the query proposal returns back to the controller using the GetShareholdersBySql function.
+
+For security reasons, you cannot use a SQL query directly from www. Therefore, GetSQLCreateFromProposal analyzes the proposals, extracts parameters from them and creates an SQL query itself. Next, GetShareholdersBySql executes this query via EF and returns a filtered list to the www page. There, Ajax clearly displays this list in the DataTable.
+
